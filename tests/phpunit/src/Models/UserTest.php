01ddad68f457bc01e19cccc867bfad492803b9cc
Xenofon Spafaridis <NohponeX@gmail.com>
2017-06-06 17:05:30 +0300

    Update php unit to 6.1 (#5)
    * Update phpunit to 6.1 and add a test to access a model
    * Remove composer run command from README.md

--- tests/phpunit/src/Models/UserTest.php
+++ tests/phpunit/src/Models/UserTest.php
@@ -3,13 +3,34 @@ declare(strict_types=1);
 
 namespace Phramework\Examples\JSONAPI\Models;
 
+use PHPUnit\Framework\TestCase;
+use Phramework\JSONAPI\Filter;
+use Phramework\JSONAPI\Page;
+
 /**
  * @license https://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  * @author Xenofon Spafaridis <nohponex@gmail.com>
  * @coversDefaultClass \Phramework\Examples\JSONAPI\Models\User
  */
-class UserTest extends \PHPUnit_Framework_TestCase
+class UserTest extends TestCase
 {
+
+    /**
+     * This test will get the user with id 1
+     * Depends on user created at tools/database.php
+     */
+    public function testGetToReturnDefaultUser()
+    {
+        $users = User::get(
+            new Page(1),
+            new Filter(
+                [1]
+            )
+        );
+
+        $this->assertCount(1, $users);
+    }
+
     /**
      * @covers ::getType
      */
